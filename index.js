02a2a3b337a8cf2f85017c76e7e89884051e9c66
George Nick Gorzynski <31825917+g30r93g@users.noreply.github.com>
2019-03-25 20:32:59 +0000

    v0.0.18

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var PanasonicAPI = require('panasonic-viera-control/panasonicviera.js');
 var http = require('http');
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-  
     homebridge.registerAccessory("homebridge-panasonic", "Panasonic-TV", PanasonicTV);
 };
 
@@ -47,7 +45,7 @@ PanasonicTV.prototype.getServices = function() {
 
 // TV Power
 PanasonicTV.prototype.getOn = function(callback) {
-    var self = this;
+    let self = this;
 
     let getRequest = {
         host: self.HOST,
@@ -92,7 +90,7 @@ PanasonicTV.prototype.getOn = function(callback) {
 // If response recieved, TV goes from off to on
 // If request times out, TV goes from on to off
 PanasonicTV.prototype.setOn = function(isOn, callback) {
-    var self = this;
+    let self = this;
     
     if (!isOn) {
         self.log("TV does not support power on/off.");
@@ -104,9 +102,9 @@ PanasonicTV.prototype.setOn = function(isOn, callback) {
     let body = "<?xml version='1.0' encoding='utf-8'?> " +
     "<s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/' s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/'> " +
     " <s:Body> " +
-    "   <u:X_SendKey xmlns:u='urn:panasonic-com:service:p00NetworkControl:1'> " +
-    "     <X_KeyEvent>NRC_POWER-ONOFF</X_KeyEvent> " +
-    "   </u:X_SendKey> " +
+    "   <u:SendKey xmlns:u='urn:panasonic-com:service:p00NetworkControl:1'> " +
+    "     <KeyEvent>NRC_POWER-ONOFF</KeyEvent> " +
+    "   </u:SendKey> " +
     " </s:Body> " +
     "</s:Envelope>" +
     "";
@@ -119,7 +117,7 @@ PanasonicTV.prototype.setOn = function(isOn, callback) {
         headers: {
             "Content-Length": body.count,
             "Content-Type": 'text/xml; charset="utf-8"',
-            SOAPACTION: '"urn:panasonic-com:servicee:p00NetworkControl:1#X_SendKey"',
+            SOAPACTION: '"urn:panasonic-com:service:p00NetworkControl:1#SendKey"',
             Accept: "text/xml"
         }
     };
